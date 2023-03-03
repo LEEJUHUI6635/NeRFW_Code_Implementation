6a7f4e06f63cb05671d2bf507db7fce3677bc219
dlwngml <dlwngml@gmail.com>
2023-03-03 12:18:10 +0900

    JH

--- JH_main.py
+++ JH_main.py
@@ -17,8 +17,8 @@ cudnn.benchmark = True # For fast training
 parser = argparse.ArgumentParser(description='NeRF-W Implementation by JH')
 
 # LLFF
-parser.add_argument('--base_dir', type=str, default='./data/nerf_llff_data/JH_nerf_w_2_geo')
-parser.add_argument('--factor', type=int, default=1) # factor = 1 -> killed
+parser.add_argument('--base_dir', type=str, default='./data/nerf_llff_data/JH_nerf_w_20')
+parser.add_argument('--factor', type=int, default=2) # factor = 1 -> killed
 parser.add_argument('--batch_size', type=int, default=2048)
 parser.add_argument('--ndc_space', type=bool, default=True)
 
@@ -43,7 +43,7 @@ parser.add_argument('--appearance_embedded', type=bool, default=True)
 parser.add_argument('--transient_embedded', type=bool, default=True)
 parser.add_argument('--appearance_embedding_word', type=int, default=2) # appearance embedding vector의 dim = 2 -> 조도 밝 / 어
 parser.add_argument('--appearance_embedding_dim', type=int, default=48)
-parser.add_argument('--transient_embedding_word', type=int, default=56) # appearance embedding vector의 dim = 2 -> 조도 밝 / 어
+parser.add_argument('--transient_embedding_word', type=int, default=20) # appearance embedding vector의 dim = 2 -> 조도 밝 / 어
 parser.add_argument('--transient_embedding_dim', type=int, default=16)
 
 # save path
